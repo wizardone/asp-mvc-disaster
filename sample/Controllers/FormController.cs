a0e9d3b5a4ea124641eb0ab86a14e2996cc29437
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2017-02-07 22:12:49 +0200

    add forms controller

---
+++ sample/Controllers/FormController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sample.Controllers
+{
+    public class FormController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+    }
+}
